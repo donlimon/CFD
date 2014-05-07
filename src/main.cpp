a469b4ab49a69e0736b497ce9eb8b43e48a8bd51
Michael <mic.stumpf@web.de>
2014-05-07 19:27:04 +0200

    Infrastructure

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//	CFD project	
+//	Fractional step method for the incompressible	
+//	Navier-Stokes equations
+//	author: Michael Stumpf
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Hello CFD!" << endl;
+	return 0;
+}
