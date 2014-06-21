0bbb8944eb51ce15f877913bc254072fc1d32de8
Michael <mic.stumpf@web.de>
2014-06-21 23:11:01 +0200

    bugfix

--- include/settings.hpp
+++ include/settings.hpp
@@ -7,11 +7,11 @@
 
 //General settings
 const char TYPE='t';		//t: Taylor-Green, v: vortices
-const int NGP=400;			//number of grid points in one dimension 
+const int NGP=100;			//number of grid points in one dimension 
 const int TSMAX=1000;		//number of time steps
 const double DT=0.0001; 	//time step size
 const double LREF=1;		//characteristic length (=lenght of domain)
-const double DX=LREF/(NGP-1);	//grid width (grid is uniform)
+const double DX=LREF/NGP;	//grid width (grid is uniform)
 const double RE=10;			//Reynolds number
 //Taylor Green
 const double UREF=1;		//characteristic speed
